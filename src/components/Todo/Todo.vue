b202725fa369c813fae5c86788a88f67af2cfe50
atlansien <atlansien@yahoo.co.jp>
2019-07-11 16:23:45 +0900

    Create todo

---
+++ src/components/Todo/Todo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <v-card hover class="card">
+      <v-container fluid grid-list-md row xs12 sm6 md3>
+        <v-list-tile-action>
+          <v-list-tile-title v-text="todo.text"></v-list-tile-title>
+          <v-list-tile-sub-title v-text="todo.date"></v-list-tile-sub-title>
+        </v-list-tile-action>
+      </v-container>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+	props:['todo']
+}
+</script>
+
+<style scoped>
+.card {
+  border: 2px solid lightseagreen;
+  color: gray;
+  background-color: transparent;
+	margin: 10px 20px;
+
+}
+</style>
--- src/components/Todo/Todos.vue
+++ src/components/Todo/Todos.vue
@@ -1,21 +1,15 @@
 <template>
   <div>
     <v-layout row wrap justify-center>
-      <v-list-tile v-for="todo in todos" :key="todo.id">
-        <v-card hover class="card">
-          <v-container fluid grid-list-md row xs12 sm6 md3>
-            <v-list-tile-action>
-              <v-list-tile-title v-text="todo.text"></v-list-tile-title>
-              <v-list-tile-sub-title v-text="todo.date"></v-list-tile-sub-title>
-            </v-list-tile-action>
-          </v-container>
-        </v-card>
-      </v-list-tile>
+      <app-todo v-for="todo in todos" :key="todo.id" :todo="todo">
+        <v-list-tile v-for="todo in todos" :key="todo.id"></v-list-tile>
+      </app-todo>
     </v-layout>
   </div>
 </template>
 
 <script>
+import Todo from "./Todo.vue";
 import moment from "moment";
 export default {
   data() {
@@ -35,17 +29,17 @@ export default {
           id: 3,
           text: "友達と遊ぶ",
           date: moment().format("MMMM Do YYYY, h:mm:ss a")
+        },
+        {
+          id: 4,
+          text: "風呂を入れる",
+          date: moment().format("MMMM Do YYYY, h:mm:ss a")
         }
       ]
     };
+  },
+  components: {
+    appTodo: Todo
   }
 };
 </script>
-
-<style scoped>
-.card {
-	border: 2px solid lightseagreen;
-	color: gray;
-	background-color: transparent;
-}
-</style>
