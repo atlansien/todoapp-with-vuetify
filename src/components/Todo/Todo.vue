d95cbf311d54ff89bf3f4294e4ab399066ef5ff2
atlansien <atlansien@yahoo.co.jp>
2019-07-12 14:07:48 +0900

    add:TodoDialogのモーダル化

--- src/components/Todo/Todo.vue
+++ src/components/Todo/Todo.vue
@@ -22,31 +22,20 @@
         </v-layout>
       </v-card>
     </v-container>
-
-    <v-dialog v-model="todo.dialog" scrollable max-width="80%" color="primary">
-      <v-card>
-        <v-card-title class="modal-todo-title">
-          <div>{{ todo.title }}</div>
-        </v-card-title>
-        <v-divider></v-divider>
-        <v-card-text class="modal-todo-text">{{ todo.text }}</v-card-text>
-        <v-card-text class="modal-todo-date">作成日: {{ todo.date }}</v-card-text>
-        <v-divider></v-divider>
-          <v-card-actions>
-            <v-checkbox class="modal-checkbox" v-model="todo.endOfTodo"></v-checkbox>
-            <v-btn color="error">削除</v-btn>
-          </v-card-actions>
-      </v-card>
-    </v-dialog>
+		<app-todo-dialog :todo="todo"></app-todo-dialog>
   </div>
 </template>
 
 <script>
+import TodoDialog from "./dialogs/TodoDialog.vue"
 export default {
   props: ["todo"],
   data() {
     return {};
-  }
+  },
+	components: {
+		appTodoDialog: TodoDialog,
+	}
 };
 </script>
 
---
+++ src/components/Todo/dialogs/TodoDialog.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-dialog v-model="todo.dialog" scrollable max-width="50%" color="primary">
+    <v-card>
+      <v-card-title class="modal-todo-title">
+        <div>{{ todo.title }}</div>
+      </v-card-title>
+      <v-divider></v-divider>
+      <v-card-text class="modal-todo-text">{{ todo.text }}</v-card-text>
+      <v-card-text class="modal-todo-date">作成日: {{ todo.date }}</v-card-text>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-checkbox class="modal-checkbox" v-model="todo.endOfTodo"></v-checkbox>
+        <v-btn color="error">削除</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+	props:['todo']
+}
+</script>
