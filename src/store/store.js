1cf66764b5d6413abd389b3f7880065d9ee96669
atlansien <atlansien@yahoo.co.jp>
2019-07-13 16:19:26 +0900

    add:todoのデモデータ保管先変更

--- src/store/store.js
+++ src/store/store.js
@@ -3,4 +3,19 @@ import Vuex from "Vuex";
 
 Vue.use(Vuex);
 
-export const store = new Vuex.Store({});
+export const store = new Vuex.Store({
+  state: {
+    data() {
+      return {
+        todos: []
+      };
+    },
+    mutations: {},
+    actions: {},
+    getters: {
+      todos(state) {
+        return state.todos;
+      }
+    }
+  }
+});
