5db387d7782c3c8602da30d31a18e5a324e2f14e
atlansien <atlansien@yahoo.co.jp>
2019-07-17 11:51:26 +0900

    add:actions.spec.js完成

--- tests/unit/store/actions.spec.js
+++ tests/unit/store/actions.spec.js
@@ -1,1 +1,10 @@
-import actions from "@/store/getters";
+import actions from "@/store/actions";
+import todos from "@/data/todos"
+
+describe("test actions.js", () => {
+  it("test", async () => {
+    const commit = jest.fn();
+    await actions.fetchTodos({ commit });
+    expect(commit).toHaveBeenCalledWith("setTodos", todos);
+  });
+});
