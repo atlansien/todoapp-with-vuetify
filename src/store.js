4e6caad170bdbae391ac600c642e35dd530c4fff
atlansien <atlansien@yahoo.co.jp>
2019-07-17 10:20:46 +0900

    add:getters.todosの作成

--- src/store.js
+++ src/store.js
@@ -1,19 +1,15 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    todos: [],
-  },
-  mutations: {
-
-  },
-  actions: {
-
+    todos: []
   },
   getters: {
-  
+    todos(state) {
+      return state.todos;
+    }
   }
-})
+});
