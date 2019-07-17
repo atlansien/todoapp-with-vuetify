9b12d622425594161656def09b50c07a04a1ec5f
atlansien <atlansien@yahoo.co.jp>
2019-07-17 13:46:32 +0900

    add:actions.postTodoの作成

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,7 +1,18 @@
 import todos from "../data/todos";
+import moment from "moment";
+moment.locale("ja");
 
 export default {
   fetchTodos: ({ commit }) => {
     commit("setTodos", todos);
+  },
+  postTodo: ({ commit }, { title, text }) => {
+    const todo = {
+      title: title,
+      text: text,
+      date: moment().format("YYYY年 MM月 Do(ddd), kk時mm分 "),
+      endOfTodo: false
+    };
+    commit("addTodo", todo)
   }
 };
