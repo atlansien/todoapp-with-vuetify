4a06a5d7c648ef1ce7174cf7e7e620790f6fa9a5
atlansien <atlansien@yahoo.co.jp>
2019-07-18 13:47:37 +0900

    change:routerファイルの場所変更

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Top from '@/components/Top';
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '../views/Home.vue'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
-      name: 'Top',
-      component: Top
+      name: 'home',
+      component: Home
     }
   ]
-});
+})
