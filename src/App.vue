481847f3078d53315300501e30487347bd0973c7
atlansien <atlansien@yahoo.co.jp>
2019-07-18 13:36:44 +0900

    add:vue add vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,10 @@
 <template>
-  <div>
-    <v-app>
-      <app-header />
-      <v-content>
-        <app-input />
-        <app-todos />
-      </v-content>
-    </v-app>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import Header from "./components/Header.vue";
-import Input from "./components/Input.vue";
-import Todos from "./components/Todo/Todos.vue";
-export default {
-  components: {
-    appHeader: Header,
-    appInput: Input,
-    appTodos: Todos
-  },
-  created() {
-    this.$store.dispatch("fetchTodos");
-  }
-};
-</script>
 
