06c8b2a8ab033bd51c8dc7bcba4161b5641db137
atlansien <atlansien@yahoo.co.jp>
2019-07-18 17:17:22 +0900

    add:updateTodo

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -4,5 +4,10 @@ export default {
   },
   addTodo(state, todo) {
     state.todos.push(todo);
+  },
+  updateTodo(state, editTodo) {
+    const index = state.todos.findIndex(todo => todo.id = editTodo.id)
+    state.todos[index].title = editTodo.title;
+    state.todos[index].text = editTodo.text
   }
 };
