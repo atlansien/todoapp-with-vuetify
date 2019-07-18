6f2a318a0a8060fe3c0afb766168851bea8f59e1
atlansien <atlansien@yahoo.co.jp>
2019-07-18 13:54:26 +0900

    inputフォームをindex.vueに直書き

--- src/views/index.vue
+++ src/views/index.vue
@@ -3,7 +3,21 @@
     <v-app>
       <app-header />
       <v-content>
-        <app-input />
+        <v-form>
+          <v-container>
+            <v-layout justify-center>
+              <v-flex xs12 sm4 md5>
+                <v-text-field label="タイトルを入力してください" v-model="title"></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm4 md5>
+                <v-text-field label="内容を入力してください" v-model="text"></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm4 md2>
+                <v-btn outline align-center color="primary" dark>送信</v-btn>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-form>
         <app-todos />
       </v-content>
     </v-app>
@@ -12,12 +26,10 @@
 
 <script>
 import Header from "../components/Grobal/Header";
-import Input from "../components/Input.vue";
 import Todos from "../components/Todo/Todos.vue";
 export default {
   components: {
     appHeader: Header,
-    appInput: Input,
     appTodos: Todos
   },
   created() {
