91aad04433162e9b5207c75b0a756963b3a22074
atlansien <atlansien@yahoo.co.jp>
2019-07-19 09:37:22 +0900

    add:isUpdateの範囲調整

--- src/components/Todo/dialogs/TodoDialog.vue
+++ src/components/Todo/dialogs/TodoDialog.vue
@@ -1,20 +1,24 @@
 <template>
   <v-dialog v-model="isOpen" scrollable max-width="50%">
     <v-card>
-      <v-card-title class="modal-todo-title">
-        <div v-if="!isUpdate">{{ todo.title }}</div>
+      <v-card-title v-if="!isUpdate" class="modal-todo-title">
+        <div>{{ todo.title }}</div>
       </v-card-title>
-      <v-text-field v-if="isUpdate" v-model="title" label="title" required></v-text-field>
+      <v-card-actions class="update-title" v-if="isUpdate">
+        <v-text-field v-model="title" label="title" required></v-text-field>
+      </v-card-actions>
 
       <v-divider></v-divider>
       <v-card-text v-if="!isUpdate" class="modal-todo-text">{{ todo.text }}</v-card-text>
-      <v-text-field v-if="isUpdate" v-model="text" label="text" required></v-text-field>
+      <v-card-actions v-if="isUpdate">
+        <v-text-field v-model="text" label="text" required></v-text-field>
+      </v-card-actions>
       <v-card-text class="modal-todo-date">作成日: {{ todo.date }}</v-card-text>
       <v-spacer></v-spacer>
       <v-card-actions>
         <v-checkbox class="modal-checkbox" v-model="todo.endOfTodo"></v-checkbox>
         <v-btn v-if="!isUpdate" color="success" @click="openUpdateForm()" outline>編集</v-btn>
-        <v-btn v-if="isUpdate" color="error" >キャンセル</v-btn>
+        <v-btn v-if="isUpdate" color="error" @click="closeUpdateForm()">キャンセル</v-btn>
         <v-btn v-if="isUpdate" color="info" outline>変更</v-btn>
 
         <!-- メソッド作成後コメント外す -->
@@ -53,6 +57,9 @@ export default {
       this.isUpdate = true;
       this.title = this.todo.title;
       this.text = this.todo.text;
+    },
+    closeUpdateForm() {
+      this.isUpdate = false;
     }
   }
 };
@@ -74,5 +81,9 @@ export default {
   height: 34px;
   margin: 10px 0 0px 0;
   padding: 0 0 0 10px;
+}
+
+.update-title {
+  padding-bottom: 0;
 }
 </style>
