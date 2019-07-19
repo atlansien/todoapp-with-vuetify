bb52a2f9ff0e54206d45b1fa2929f3659882914b
atlansien <atlansien@yahoo.co.jp>
2019-07-19 09:09:04 +0900

    add:編集フォームへの切り替えメソッド作成(openUpdateForm)

--- src/components/Todo/dialogs/TodoDialog.vue
+++ src/components/Todo/dialogs/TodoDialog.vue
@@ -2,15 +2,18 @@
   <v-dialog v-model="isOpen" scrollable max-width="50%">
     <v-card>
       <v-card-title class="modal-todo-title">
-        <div>{{ todo.title }}</div>
+        <div v-if="!isUpdate">{{ todo.title }}</div>
       </v-card-title>
+      <v-text-field v-if="isUpdate" v-model="title" label="title" required></v-text-field>
+
       <v-divider></v-divider>
-      <v-card-text class="modal-todo-text">{{ todo.text }}</v-card-text>
+      <v-card-text v-if="!isUpdate" class="modal-todo-text">{{ todo.text }}</v-card-text>
+      <v-text-field v-if="isUpdate" v-model="text" label="text" required></v-text-field>
       <v-card-text class="modal-todo-date">作成日: {{ todo.date }}</v-card-text>
       <v-spacer></v-spacer>
       <v-card-actions>
         <v-checkbox class="modal-checkbox" v-model="todo.endOfTodo"></v-checkbox>
-        <v-btn color="success">text</v-btn>
+        <v-btn color="success" @click="openUpdateForm()">text</v-btn>
         <!-- メソッド作成後コメント外す -->
         <!-- <v-btn color="red" flat @click="todo.deleteDialog=true">削除</v-btn> -->
       </v-card-actions>
@@ -31,7 +34,9 @@ export default {
   },
   data() {
     return {
-      isOpen: false
+      text: "",
+      isOpen: false,
+      isUpdate: false
     };
   },
   methods: {
@@ -40,6 +45,11 @@ export default {
     },
     close() {
       this.isOpen = false;
+    },
+    openUpdateForm() {
+      this.isUpdate = true;
+      this.title = this.todo.title;
+      this.text = this.todo.text;
     }
   }
 };
