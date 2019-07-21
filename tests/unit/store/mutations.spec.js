0a648c34e7ee6972f0d6967d52b9d622d63aeaad
atlansien <atlansien@yahoo.co.jp>
2019-07-21 23:50:05 +0900

    add:mutations.spec.jsのtodoの作成方法変更、またそれに伴いテスト内容も変更

--- tests/unit/store/mutations.spec.js
+++ tests/unit/store/mutations.spec.js
@@ -6,26 +6,54 @@ const state = {
   todos: []
 };
 
+let nextID = 1;
+
+class Todo {
+  constructor({ title, text }) {
+    this.id = nextID++;
+    this.title = title;
+    this.text = text;
+    this.date = moment().format("YYYY年 MM月 Do(ddd), kk時mm分 ");
+    this.completed = false;
+  }
+}
+
 describe("test mutations.js", () => {
   it("mutations.setTodosはstate.todosに第二引数を渡す", () => {
-    const todos = [
-      { id: 1, title: "test title1", text: "test text1" },
-      { id: 2, title: "test title2", text: "test text2" }
-    ];
+    const todos = [];
+
+    for (let i = 0; i < 5; i++) {
+      const todo = new Todo({
+        title: "タイトル" + i,
+        text: `詳細分` + i
+      });
+      todos.push({ ...todo });
+    }
     mutations.setTodos(state, todos);
     expect(state.todos).toEqual(todos);
   });
   it("mutations.addTodoはstate.todosの末尾に第二引数をpushする", () => {
-    const todo = { id: 3, title: "test title3", text: "test text3" };
+    const todo = new Todo({
+      title: "タイトル" + nextID,
+      text: "詳細分" + nextID
+    });
 
-    mutations.addTodo(state, todo);
-    expect(state.todos[2]).toEqual(todo);
+    const todoId = nextID - 2;
+
+    mutations.addTodo(state, { ...todo });
+    expect(state.todos[todoId]).toEqual(todo);
   });
   it("mutations.updateTodoは指定したidと紐つく配列内のtodoを内容を変更する", () => {
-    const todo = { id: 3, title: "update title", text: "update text" };
+    const id = 3
 
+    const oldTodo = state.todos.find(todo => id === todo.id)
+
+    const todo = { id: id, title: "update title", text: "update text" };
     mutations.updateTodo(state, todo);
-    expect(state.todos[2]).toEqual(todo);
+
+    const updatedTodo = state.todos.find(todo => id === todo.id)
+
+    expect(oldTodo).toEqual(updatedTodo);
   });
   it("mutations.removeTodoは指定したidと紐つく配列内のtodoを一件削除する", () => {
     const oldTodos = state.todos.slice();
